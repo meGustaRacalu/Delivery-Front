c0d9923f146d8765097c802f6914aba727c4023c
User <raquelmorabito@hotmail.com>
2025-01-30 08:00:01 -0300

    ajuste de preco no card produtos

--- src/components/produtos/cardprodutos/CardProdutos.tsx
+++ src/components/produtos/cardprodutos/CardProdutos.tsx
@@ -1,18 +1,16 @@
-import { Link } from 'react-router-dom';
-import Produto from '../../../models/Produtos';
-
-interface CardProdutosProps {
-    produto: Produto;
-}
-
 function CardProdutos({ produto }: CardProdutosProps) {
+    const formatCurrency = (value: number) => {
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        }).format(value);
+    };
+
     return (
         <div className='border-slate-900 border 
             flex flex-col rounded overflow-hidden justify-between'>
-                
             <div>
                 <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
-                    {/* Corrigido: agora usa imagemUrl */}
                     {produto.imagemUrl && (
                         <img
                             src={produto.imagemUrl}
@@ -24,11 +22,11 @@ function CardProdutos({ produto }: CardProdutosProps) {
                         {produto.nome}
                     </h3>
                 </div>
-                <div className='p-4 '>
+                <div className='p-4'>
                     <h4 className='text-lg font-semibold uppercase'>Informações do Produto</h4>
-                    <p>Preço: R$ {produto.preco.toFixed(2)}</p>
+                    <p>Preço: {formatCurrency(produto.preco)}</p>
                     <p>Categoria: {produto.categoria}</p>
-                    <p>Taxa de Entrega: R$ {produto.entrega.toFixed(2)}</p>
+                    <p>Taxa de Entrega: {formatCurrency(produto.entrega)}</p>
                 </div>
             </div>
             <div className="flex">
@@ -46,5 +44,3 @@ function CardProdutos({ produto }: CardProdutosProps) {
         </div>
     );
 }
-
-export default CardProdutos;
