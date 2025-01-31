84d7a1daa1197077fff5aa13f07d710c812d23f9
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2025-01-30 22:07:03 -0300

    Setup Deploy

--- src/pages/produtos/ProdutosSaudaveis.tsx
+++ src/pages/produtos/ProdutosSaudaveis.tsx
@@ -17,7 +17,9 @@ function ProdutosSaudaveis() {
 
     return (
         <main className='flex-grow'>
+            <h1 className="text-2xl font-bold text-orange-800">Produtos Saudáveis</h1>
             <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+
                 {produtos.map(produto => (
                     <CardProdutos key={produto.id} produto={produto} />
                 ))}
